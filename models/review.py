a0e1046753e87cd4a23a89566e1a20fca3fbf0c3
tabby400 <tabbynargaret@gmail.com>
2023-09-20 15:27:42 -0700

    review

--- models/review.py
+++ models/review.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from os import getenv
+from sqlalchemy.sql.schema import ForeignKey
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """this is the review class for review info """
+    if storage_type == 'db':
+        __tablename__ = "reviews"
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        text = Column(String(1024), nullable=False)
