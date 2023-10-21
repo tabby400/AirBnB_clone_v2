fa942b02b1dd6a3517400c0ef4d46944b171b7e8
tabby400 <tabbynargaret@gmail.com>
2023-10-21 04:00:24 -0700

    cities

--- models/state.py
+++ models/state.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
 """ this is state module """
-
+import models
 from models.base_model import BaseModel, Base
 from models import storage_t
 from models.city import City
-from sqlalchemy import Column, String
+import sqlalchemy
+from os import getenv
+from models.city import City
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ class state"""
-
-    __tablename__ = 'states'
-    if storage_t == 'db':
+    if models.storage_t == "db":
+        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state',
                               cascade='all, delete, delete-orphan')
     else:
         name = ''
+    
+    def __init__(self, *args, **kwargs):
+        """initializing state"""
+        super().__init__(*args, **kwargs)
 
+    
     if models.storage_t != "db":
-
         @property
         def cities(self):
             '''returning list of City instances with state_id
@@ -30,7 +36,7 @@ class State(BaseModel, Base):
 
             from models import storage
             related_cities = []
-            cities = storage.all(City)
+            cities = models.storage.all(City)
             for city in cities.values():
                 if city.state_id == self.id:
                     related_cities.append(city)
