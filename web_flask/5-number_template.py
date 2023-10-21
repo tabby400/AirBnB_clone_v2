1ac493641e6ab65a16263f4389016d309bfd74f0
tabby400 <tabbynargaret@gmail.com>
2023-10-21 01:45:01 -0700

    render html

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+"""This uses the flask framework"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """btings hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """brings  HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_c(text):
+    """brings back  the specific text given"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def show_text(text):
+    """this shows python the the text value"""
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def show_number(n):
+    """ shows number(n) only if integer"""
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ rendering a html page"""
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == '__main__':  # executed directly
+    app.run(host='0.0.0.0', port=5000)
