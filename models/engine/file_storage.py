157b15c7140101f8f11aea93af3d563751f01311
tabby400 <tabbynargaret@gmail.com>
2023-10-21 04:01:24 -0700

    storagge

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -43,6 +43,15 @@ class FileStorage:
 
     def reload(self):
         """Loading storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+
         classes = {
             'BaseModel': BaseModel, 'User': User, 'Place': Place,
             'State': State, 'City': City, 'Amenity': Amenity,
@@ -59,7 +68,7 @@ class FileStorage:
 
     def close(self):
         """displaying our HBNB data"""
-        self.reload()
+        Filestorage.reload()
 
     def delete(self, obj=None):
         """deleting an object"""
