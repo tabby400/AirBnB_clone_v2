365d7c5d55742c629e7a57c9d5c5a37d1e88c295
tabby400 <tabbynargaret@gmail.com>
2023-10-21 01:14:59 -0700

    display c

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This uses the flask framework"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """btings hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """brings  HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_c(text):
+    """brings back  the specific text given"""
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == '__main__':  # executed directly
+    app.run(host='0.0.0.0', port=5000)
