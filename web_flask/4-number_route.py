b47ba04c7fac2c7048dfa6fa4f5ad85200e09560
tabby400 <tabbynargaret@gmail.com>
2023-10-21 01:30:21 -0700

    integer

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""This uses the flask framework"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """btings hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """brings  HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_c(text):
+    """brings back  the specific text given"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def show_text(text):
+    """this shows python the the text value"""
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def show_number(n):
+    """ shows number(n) only if integer"""
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':  # executed directly
+    app.run(host='0.0.0.0', port=5000)
