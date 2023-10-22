6d2c18de049e202ca4dca86a88fbc6a69cf36b68
tabby400 <tabbynargaret@gmail.com>
2023-10-22 10:13:44 -0700

    filters

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""flask web framework used"""
+
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """hBnB filters HTML page."""
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    return render_template("10-hbnb_filters.html",
+                           states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Current SQLAlchemy session is out."""
+    storage.close()
+
+
+if __name__ == "__main__":  # run directly
+    app.run(host="0.0.0.0", port=5000)
